a25c9c103e0d2f6f13954401d428983bbf68ba0f
khanhtd <khanhtd@KHANH>
2023-07-27 16:17:05 +0700

    ok
    model mail zalo

---
+++ EVN.Api/Model/EmailModel.cs
@@ -0,0 +1,27 @@
+﻿using EVN.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EVN.Api.Model
+{
+    public class ZaloModel
+    {
+        public string MA_DVIQLY { get; set; }
+        public string MA_KHANG { get; set; }
+        public string MA_DVU { get; set; }
+        public string EMAIL { get; set; }
+        public string ID_ZALO { get; set; }
+        public string NOI_DUNG { get; set; }
+        public int TINH_TRANG { get; set; }
+        public DateTime NGAY_TAO { get; set; }
+        public string NGUOI_TAO { get; set; }
+        public DateTime NGAY_SUA { get; set; }
+        public string NGUOI_SUA { get; set; }
+        public int ID_HDON { get; set; }
+        public string TIEU_DE { get; set; }
+
+
+    }
+}
