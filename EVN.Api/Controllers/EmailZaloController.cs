64f714951aac0fd7776403c71f41ef840eaec6d8
khanhtd <khanhtd@KHANH>
2023-08-11 16:18:37 +0700

    ok

--- EVN.Api/Controllers/EmailZaloController.cs
+++ EVN.Api/Controllers/EmailZaloController.cs
@@ -119,41 +119,6 @@ namespace EVN.Api.Controllers
             }
         }
 
-        //[HttpPost]
-        //[Route("zalo/add")]
-        //public IHttpActionResult AddZalo([FromBody] ZaloModelFilter request)
-        //{
-        //    ResponseFileResult result = new ResponseFileResult();
-        //    try
-        //    {
-        //        IZaloService service = IoC.Resolve<IZaloService>();
-        //        Zalo zalo = new Zalo();
-        //        zalo.ID = request.Filter.ID;
-        //        zalo.MA_DVIQLY = request.Filter.MA_DVIQLY;
-        //        zalo.MA_DVU = request.Filter.MA_DVU;
-        //        zalo.MA_KHANG = request.Filter.MA_KHANG;
-        //        zalo.NOI_DUNG = request.Filter.NOI_DUNG;
-        //        zalo.NGAY_SUA = request.Filter.NGAY_SUA;
-        //        zalo.NGUOI_SUA = request.Filter.NGUOI_SUA;
-        //        zalo.NGAY_TAO = request.Filter.NGAY_TAO;
-        //        zalo.NGUOI_TAO = request.Filter.NGUOI_TAO;
-        //        zalo.TIEU_DE = request.Filter.TIEU_DE;
-        //        zalo.TINH_TRANG = request.Filter.TINH_TRANG;
-        //        zalo.ID_ZALO = request.Filter.ID_ZALO;
-        //        zalo.ID_HDON = request.Filter.ID_HDON;
-        //        service.CreateNew(zalo);
-        //        service.CommitChanges();
-        //        result.success = true;
-        //        result.message = "Thêm mới thành công";
-        //        return Ok(result);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        log.Error(ex);
-        //        result.success = false;
-        //        result.message = ex.Message;
-        //        return Ok(result);
-        //    }
-        //}
+     
     }
 }
---
+++ EVN.Core/Models/APIException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVN.Core.Models
+{
+    public class APIException : Exception
+    {
+        private int _code;
+
+        public int code { get; set; }
+
+        public APIException() : base() { }
+
+        public APIException(string message) : base(message) { }
+    }
+}
